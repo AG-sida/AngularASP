aa48fe2c2d1512b5254a24176b2c91b89593383d
Leonardo Lazcano <Leonardo Lazcano>
2017-10-06 19:43:47 +0200

    Add project files.

---
+++ AngularASP/ClientApp/app/components/counter2/counter2.component.ts
@@ -0,0 +1,16 @@
+﻿import { Component } from '@angular/core';
+
+@Component({
+    selector: 'counter2',
+    templateUrl: './counter2.component.html'
+})
+export class Counter2Component {
+    public currentCount = 0;
+
+    public incrementCounter() {
+        this.currentCount++;
+    }
+    public decrementCounter() {
+        this.currentCount--;
+    }
+}
